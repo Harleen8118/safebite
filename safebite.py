761e2f065100d0a15e8450ee301b910fd5755a5e
Harleen8118 <harleenbbagga@gmail.com>
2025-05-03 16:55:27 +0530

    update

--- safebite.py
+++ safebite.py
@@ -79,3 +79,4 @@ def clean_ingredients(text):
     ingredient_text = re.sub(r'contains \d+% or less of\s*[:]*\s*', '', ingredient_text, flags=re.IGNORECASE)
 
     potential_ingredients = re.split(r'[;,]\s*(?![^()]*\))|\.\s+(?![^()]*\))|\s+and\s+(?![^()]*\))', ingredient_text)
+
