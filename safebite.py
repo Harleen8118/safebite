94da20953304aa4ca4f14814eddf9aec4804c116
Harleen8118 <harleenbbagga@gmail.com>
2025-05-03 16:41:08 +0530

    update

--- safebite.py
+++ safebite.py
@@ -7,3 +7,6 @@ from symspellpy import SymSpell, Verbosity
 import pytesseract
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, TextStreamer
 
+# --- Configuration ---
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+IMAGE_PATH = "/content/Maggie.jpg" # Replace with your image path
